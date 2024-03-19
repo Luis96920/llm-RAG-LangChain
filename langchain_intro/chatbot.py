6ff13399bd89bf8c34efbc3659f0cc467219bd9b
luis96920 <lucasm3938@gmail.com>
2024-03-19 04:55:22 -0700

    Created review_prompt_template and chained to chat_model.

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -1,7 +1,44 @@
-import dotenv # Import the dotenv package to load environment variables
+import dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
 
-from langchain_openai import ChatOpenAI # Import the ChatOpenAI class from langchain_openai package
+dotenv.load_dotenv()
 
-dotenv.load_dotenv() # Load environment variables from .env file
+review_template_str = """Your job is to use patient
+reviews to answer questions about their experience at
+a hospital. Use the following context to answer questions.
+Be as detailed as possible, but don't make up any information
+that's not from the context. If you don't know an answer, say
+you don't know.
 
-chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0) # Create a chatbot model
+{context}
+"""
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        template=review_template_str,
+    )
+)
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        template="{question}",
+    )
+)
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context", "question"],
+    messages=messages,
+)
+
+chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+
+review_chain = review_prompt_template | chat_model
