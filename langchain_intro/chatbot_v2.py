9a432af61056c82ff8aa94b31443fdbdd7a58b13
luis96920 <lucasm3938@gmail.com>
2024-03-30 11:46:39 -0700

    Added tools.py and chat agent to chatbot_v2.py.

--- langchain_intro/chatbot_v2.py
+++ langchain_intro/chatbot_v2.py
@@ -16,6 +16,14 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.schema.runnable import RunnablePassthrough
 
+from langchain.agents import(
+    create_openai_functions_agent,
+    Tool,
+    AgentExecutor,
+)
+from langchain import hub
+from langchain_intro.tools import get_current_wait_time
+
 REVIEWS_CHROMA_PATH = "chroma_data/"
 
 # Load the environment variables
@@ -76,3 +84,52 @@ review_chain = (
     | chat_model
     | StrOutputParser()
 )
+
+tools = [
+    Tool(
+        name="Reviews",
+        func=review_chain.invoke,
+        description="""Useful when you need to answer questions
+        about patient reviews or experiences at the hospital.
+        Not useful for answering questions about specific visit
+        details such as payer, billing, treatment, diagnosis,
+        chief complaint, hospital, or physician information.
+        Pass the entire question as input to the tool. For instance,
+        if the question is "What do patients think about the triage system?",
+        the input should be "What do patients think about the triage system?"
+        """,
+    ),
+    Tool(
+        name="Waits",
+        func=get_current_wait_time,
+        description="""Use when asked about current wait times
+        at a specific hospital. This tool can only get the current
+        wait time at a hospital and does not have any information about
+        aggregate or historical wait times. This tool returns wait times in
+        minutes. Do not pass the word "hospital" as input,
+        only the hospital name itself. For instance, if the question is
+        "What is the wait time at hospital A?", the input should be "A".
+        """,
+    ),
+]
+
+hospital_agent_prompt = hub.pull("hwchase17/openai-functions-agent")
+
+agent_chat_model = ChatOpenAI(
+    model="gpt-3.5-turbo-1106",
+    temperature=0,
+)
+
+hospital_agent = create_openai_functions_agent(
+    llm=agent_chat_model,
+    prompt=hospital_agent_prompt,
+    tools=tools,
+)
+
+hospital_agent_executor = AgentExecutor(
+    agent=hospital_agent,
+    tools=tools,
+    return_intermediate_steps=True,
+    verbose=True,
+)
+
